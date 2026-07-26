6afac257f0e2b6380e9fe8225983e38b1515760c
gtmnow <74516099+gtmnow@users.noreply.github.com>
2026-07-26 17:46:25 -0600

    Fix asset paths for custom domain

--- vite.pages.config.ts
+++ vite.pages.config.ts
@@ -1,13 +1,8 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
-const repositoryName = process.env.GITHUB_REPOSITORY?.split("/")[1];
-const isProjectSite =
-  Boolean(repositoryName) && !repositoryName?.endsWith(".github.io");
-const base = isProjectSite ? `/${repositoryName}/` : "/";
-
 export default defineConfig({
-  base,
+  base: "/",
   root: "github-pages-src",
   publicDir: "../public",
   plugins: [react()],
