800b8487d82093056a508c0c027d9a8b6b21b14b
gtmnow <74516099+gtmnow@users.noreply.github.com>
2026-07-26 16:11:27 -0600

    Set intro frames to fifteen seconds

--- app/DiscoveryExperience.tsx
+++ app/DiscoveryExperience.tsx
@@ -95,8 +95,8 @@ export function DiscoveryExperience() {
       setIntroStage((current) =>
         current < introBeats.length - 1 ? current + 1 : current,
       );
-    }, 7200);
-    const closeTimer = window.setTimeout(() => setIntroOpen(false), 31950);
+    }, 15000);
+    const closeTimer = window.setTimeout(() => setIntroOpen(false), 60000);
 
     return () => {
       window.clearInterval(stageTimer);
